a6726ec8df435792993a1bbf702efbc080d31dd0
durgesh730 <durgesh.unicoglobal@gmail.com>
2024-12-27 20:12:51 +0530

    jon logic change

--- src/models/jobApplicants.model.js
+++ src/models/jobApplicants.model.js
@@ -18,7 +18,7 @@ const jobApplicantSchema = new mongoose.Schema({
             /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
             "Please provide a valid email address",
         ],
-        unique: true,
+        unique: false,
         lowercase: true,
         required: [true, "Please provide a email address"],
     },
